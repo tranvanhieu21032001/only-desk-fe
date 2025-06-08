0751d32ab39ce03a8d23a5e021ddab8a2ac91725
tranvanhieu21032001 <tranvanhieu21032001@gmail.com>
2025-06-09 00:07:27 +0700

    integrate translations in billing module

--- src/modules/settings/pages/billing/change-plan/Content/Choise-plan/ChoisePlan.tsx
+++ src/modules/settings/pages/billing/change-plan/Content/Choise-plan/ChoisePlan.tsx
@@ -1,162 +1,185 @@
 import { Image, Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 import * as S from './ChoisePlan.styles';
 import Button from '@/shared/components/common/Button';
+
 import icCheck from '@/assets/icons/billing/ic-tick-circle2.svg';
 import icCheckDark from '@/assets/icons/billing/ic-tick-circle-dark.svg';
 import icFlat from '@/assets/icons/billing/ic-flat.svg';
 import icTime from '@/assets/icons/billing/ic-time.svg';
 import icTransfer from '@/assets/icons/billing/ic-transfer.svg';
 import icTransferDark from '@/assets/icons/billing/ic-transfer-dark.svg';
-
 import icMinitor from '@/assets/icons/billing/ic-monitor.svg';
 import icUser from '@/assets/icons/billing/ic-user.svg';
 import icInfor from '@/assets/icons/billing/ic-info.svg';
-
 import icMinitorDark from '@/assets/icons/billing/ic-monitor-dark.svg';
 import icUserDark from '@/assets/icons/billing/ic-user-dark.svg';
 import icInforDark from '@/assets/icons/billing/ic-info-dark.svg';
 
-
 const ChoisePlan = ({ setActiveStep }: { setActiveStep: (step: number) => void }) => {
-    const labels = [
-        { icon: icCheck, label: '14 days trial' },
-        { icon: icFlat, label: 'Flat pricing' },
-        { icon: icTime, label: 'Cancel anytime' },
-    ];
+  const { t } = useTranslation('billing');
 
-    const handleBuyNow = () => {
-        setActiveStep(1);
-    };
+  const labels = [
+    { icon: icCheck, label: t('choise-plan.label-trial') },
+    { icon: icFlat, label: t('choise-plan.label-flat') },
+    { icon: icTime, label: t('choise-plan.label-cancel') },
+  ];
 
-    const plans = [
-        {
-            title: "Free",
-            desc: "For solopreneurs and entrepreneurs. Your first shared inbox",
-            price: "0",
-            sections: [
-                "Chat widget",
-                "Shared inbox",
-                "Contact form",
-                "Mobile applications",
-                "Unlimited conversations",
-                "E-Commerce integrations"
-            ]
-        },
-        {
-            title: "Mini",
-            desc: "For early-stage companies who want to improve their customer service.",
-            price: "45",
-            sections: [
-                "Chat widget",
-                "Shared inbox",
-                "Contact form",
-                "Mobile applications",
-                "Unlimited conversations",
-                "E-Commerce integrations"
-            ]
-        },
-        {
-            title: "Essentials",
-            desc: "For small companies requiring a full featured customer support platform.",
-            price: "95",
-            sections: [
-                "Chat widget",
-                "Shared inbox",
-                "Contact form",
-                "Mobile applications",
-                "Unlimited conversations",
-                "E-Commerce integrations"
-            ]
-        },
-        {
-            title: "Plus",
-            desc: "For medium-sized businesses looking for an AI-powered omnichannel customer support software, plus all our features.",
-            price: "295",
-            sections: [
-                "Chat widget",
-                "Shared inbox",
-                "Contact form",
-                "Mobile applications",
-                "Unlimited conversations",
-                "E-Commerce integrations"
-            ]
-        },
-    ];
-    return (
-        <>
-            <S.PlanContainer>
-                <S.GroupLabels>
-                    {labels.map((item, idx) => (
-                        <S.Label key={idx}>
-                            <Image preview={false} src={item.icon} />
-                            <Typography>{item.label}</Typography>
-                        </S.Label>
-                    ))}
-                </S.GroupLabels>
+  const handleBuyNow = () => {
+    setActiveStep(1);
+  };
 
-                <S.PlanList>
-                    {plans.map((plan, index) => (
-                        <S.WrapPlanCard>
-                            <S.PlanCard key={index} isDark={index === plans.length - 1}>
-                                <S.PlanTitle>{plan.title}</S.PlanTitle>
-                                <S.PlanDesc>{plan.desc}</S.PlanDesc>
-                                <S.PlanPriceGroup>
-                                    <S.PlanPrice isDark={index === plans.length - 1}>${plan.price}</S.PlanPrice>
-                                    <span>/month</span>
-                                </S.PlanPriceGroup>
-                                {index !== 0 ? (
-                                    <S.GroupButton>
-                                        {
-                                            index === plans.length - 1 ? <Button
-                                                onClick={handleBuyNow}
-                                                type='default'
-                                                iconPosition='right'
-                                                icon={<Image preview={false} src={icTransferDark} />}
-                                            >
-                                                Buy now
-                                            </Button> : <Button
-                                                onClick={handleBuyNow}
-                                                type='primary'
-                                                iconPosition='right'
-                                                icon={<Image preview={false} src={icTransfer} />}
-                                            >
-                                                Buy now
-                                            </Button>
-                                        }
-                                    </S.GroupButton>
-                                ) : (
-                                    <S.GroupButton>
-                                        <S.Placholder></S.Placholder>
-                                    </S.GroupButton>
-                                )}
-                                <hr />
-                                <S.Details>
-                                    <S.DetailItem>
-                                        {index !== plans.length - 1 ? <Image preview={false} src={icMinitor} /> : <Image preview={false} src={icMinitorDark} />}
-                                        2 seat included  {index !== plans.length - 1 ? <Image preview={false} src={icInfor} /> : <Image preview={false} src={icInforDark} />}
-                                    </S.DetailItem>
-                                    <S.DetailItem>
-                                        {index !== plans.length - 1 ? <Image preview={false} src={icUser} /> : <Image preview={false} src={icUserDark} />}
-                                        100 user profiles included {index !== plans.length - 1 ? <Image preview={false} src={icInfor} /> : <Image preview={false} src={icInforDark} />}
-                                    </S.DetailItem>
-                                </S.Details>
-                                <hr />
-                                <S.SectionList isDark={index === plans.length - 1}>
-                                    {plan.sections.map((section, idx) => (
-                                        <li key={idx}>
-                                            {index !== plans.length - 1 ? <Image preview={false} src={icCheck} /> : <Image preview={false} src={icCheckDark} />}
-                                            {section}
-                                        </li>
-                                    ))}
-                                </S.SectionList>
-                            </S.PlanCard>
-                        </S.WrapPlanCard>
-                    ))}
+  const plans = [
+    {
+      title: t('choise-plan.plan-free-title'),
+      desc: t('choise-plan.plan-free-desc'),
+      price: '0',
+      sections: [
+        t('choise-plan.feature-chat-widget'),
+        t('choise-plan.feature-shared-inbox'),
+        t('choise-plan.feature-contact-form'),
+        t('choise-plan.feature-mobile-apps'),
+        t('choise-plan.feature-unlimited-conversations'),
+        t('choise-plan.feature-ecommerce'),
+      ],
+    },
+    {
+      title: t('choise-plan.plan-mini-title'),
+      desc: t('choise-plan.plan-mini-desc'),
+      price: '45',
+      sections: [
+        t('choise-plan.feature-chat-widget'),
+        t('choise-plan.feature-shared-inbox'),
+        t('choise-plan.feature-contact-form'),
+        t('choise-plan.feature-mobile-apps'),
+        t('choise-plan.feature-unlimited-conversations'),
+        t('choise-plan.feature-ecommerce'),
+      ],
+    },
+    {
+      title: t('choise-plan.plan-essentials-title'),
+      desc: t('choise-plan.plan-essentials-desc'),
+      price: '95',
+      sections: [
+        t('choise-plan.feature-chat-widget'),
+        t('choise-plan.feature-shared-inbox'),
+        t('choise-plan.feature-contact-form'),
+        t('choise-plan.feature-mobile-apps'),
+        t('choise-plan.feature-unlimited-conversations'),
+        t('choise-plan.feature-ecommerce'),
+      ],
+    },
+    {
+      title: t('choise-plan.plan-plus-title'),
+      desc: t('choise-plan.plan-plus-desc'),
+      price: '295',
+      sections: [
+        t('choise-plan.feature-chat-widget'),
+        t('choise-plan.feature-shared-inbox'),
+        t('choise-plan.feature-contact-form'),
+        t('choise-plan.feature-mobile-apps'),
+        t('choise-plan.feature-unlimited-conversations'),
+        t('choise-plan.feature-ecommerce'),
+      ],
+    },
+  ];
 
-                </S.PlanList>
-            </S.PlanContainer>
-        </>
-    )
-}
+  return (
+    <S.PlanContainer>
+      <S.GroupLabels>
+        {labels.map((item, idx) => (
+          <S.Label key={idx}>
+            <Image preview={false} src={item.icon} />
+            <Typography>{item.label}</Typography>
+          </S.Label>
+        ))}
+      </S.GroupLabels>
 
-export default ChoisePlan
+      <S.PlanList>
+        {plans.map((plan, index) => (
+          <S.WrapPlanCard key={index}>
+            <S.PlanCard isDark={index === plans.length - 1}>
+              <S.PlanTitle>{plan.title}</S.PlanTitle>
+              <S.PlanDesc>{plan.desc}</S.PlanDesc>
+              <S.PlanPriceGroup>
+                <S.PlanPrice isDark={index === plans.length - 1}>
+                  ${plan.price}
+                </S.PlanPrice>
+                <span>{t('choise-plan.month')}</span>
+              </S.PlanPriceGroup>
+
+              {index !== 0 ? (
+                <S.GroupButton>
+                  {index === plans.length - 1 ? (
+                    <Button
+                      onClick={handleBuyNow}
+                      type="default"
+                      iconPosition="right"
+                      icon={<Image preview={false} src={icTransferDark} />}
+                    >
+                      {t('choise-plan.buy-now')}
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={handleBuyNow}
+                      type="primary"
+                      iconPosition="right"
+                      icon={<Image preview={false} src={icTransfer} />}
+                    >
+                      {t('choise-plan.buy-now')}
+                    </Button>
+                  )}
+                </S.GroupButton>
+              ) : (
+                <S.GroupButton>
+                  <S.Placholder></S.Placholder>
+                </S.GroupButton>
+              )}
+
+              <hr />
+              <S.Details>
+                <S.DetailItem>
+                  <Image
+                    preview={false}
+                    src={index !== plans.length - 1 ? icMinitor : icMinitorDark}
+                  />
+                  {t('choise-plan.seats-included')}
+                  <Image
+                    preview={false}
+                    src={index !== plans.length - 1 ? icInfor : icInforDark}
+                  />
+                </S.DetailItem>
+                <S.DetailItem>
+                  <Image
+                    preview={false}
+                    src={index !== plans.length - 1 ? icUser : icUserDark}
+                  />
+                  {t('choise-plan.profiles-included')}
+                  <Image
+                    preview={false}
+                    src={index !== plans.length - 1 ? icInfor : icInforDark}
+                  />
+                </S.DetailItem>
+              </S.Details>
+
+              <hr />
+              <S.SectionList isDark={index === plans.length - 1}>
+                {plan.sections.map((section, idx) => (
+                  <li key={idx}>
+                    <Image
+                      preview={false}
+                      src={index !== plans.length - 1 ? icCheck : icCheckDark}
+                    />
+                    {section}
+                  </li>
+                ))}
+              </S.SectionList>
+            </S.PlanCard>
+          </S.WrapPlanCard>
+        ))}
+      </S.PlanList>
+    </S.PlanContainer>
+  );
+};
+
+export default ChoisePlan;
--- src/modules/settings/pages/billing/change-plan/Content/Payment/payment-method/CreditCardMethod.tsx
+++ src/modules/settings/pages/billing/change-plan/Content/Payment/payment-method/CreditCardMethod.tsx
@@ -1,110 +1,114 @@
 import React from 'react';
+import { Image } from 'antd';
+import { useTranslation } from 'react-i18next';
+
 import * as S from './CreditCardMethod.styles';
 import fontWeight from '@/shared/styles/themes/default/fontWeight';
 import Typography from '@/shared/components/common/Typography';
 import Input from '@/shared/components/common/Input';
-import { Image } from 'antd';
 
 import icValid from '@/assets/icons/knowledge-base/ic-valid.svg';
 
 const CreditCardMethod = () => {
+  const { t } = useTranslation('billing');
+
   return (
     <S.BillingDetails>
       <S.Section>
-        <S.Title>Your billing details</S.Title>
+        <S.Title>{t('credit-method.billing-details')}</S.Title>
         <hr />
         <S.FormField>
           <Typography fontWeight={fontWeight.medium}>
             <S.FormInput>
-              Address
+              {t('credit-method.address')}
               <Image preview={false} src={icValid} height={23} width={7} />
             </S.FormInput>
           </Typography>
-          <Input placeholder="Enter your address" size="large" />
+          <Input placeholder={t('credit-method.address-placeholder')} size="large" />
         </S.FormField>
+
         <S.GroupField>
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Country
+                {t('credit-method.country')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <S.ChangeCountry placeholder="Select country">
-              <S.CountryOption value="vn">Vietnam</S.CountryOption>
-              <S.CountryOption value="us">United States</S.CountryOption>
-              <S.CountryOption value="uk">United Kingdom</S.CountryOption>
+            <S.ChangeCountry placeholder={t('credit-method.country-placeholder')}>
+              <S.CountryOption value="vn">{t('credit-method.country-vn')}</S.CountryOption>
+              <S.CountryOption value="us">{t('credit-method.country-us')}</S.CountryOption>
+              <S.CountryOption value="uk">{t('credit-method.country-uk')}</S.CountryOption>
             </S.ChangeCountry>
           </S.FormField>
 
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Zip Code
+                {t('credit-method.zip-code')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="Enter zip code" size="large" />
+            <Input placeholder={t('credit-method.zip-code-placeholder')} size="large" />
           </S.FormField>
         </S.GroupField>
 
         <S.FormField>
           <Typography fontWeight={fontWeight.medium}>
             <S.FormInput>
-              Information for invoices
+              {t('credit-method.invoice-info')}
               <Image preview={false} src={icValid} height={23} width={7} />
             </S.FormInput>
           </Typography>
-          <Input placeholder="Enter your company name" size="large" />
+          <Input placeholder={t('credit-method.company-placeholder')} size="large" />
         </S.FormField>
-
 
         <S.GroupField>
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Email to send invoices to
+                {t('credit-method.email-invoice')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="Enter you email" size="large" />
+            <Input placeholder={t('credit-method.email-placeholder')} size="large" />
           </S.FormField>
 
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-               Phone number
+                {t('credit-method.phone')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="Enter your phone number" size="large" />
+            <Input placeholder={t('credit-method.phone-placeholder')} size="large" />
           </S.FormField>
         </S.GroupField>
       </S.Section>
 
       <S.Section>
-        <S.Title>Payment details</S.Title>
+        <S.Title>{t('credit-method.payment-details')}</S.Title>
         <hr />
 
         <S.GroupField>
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Card number
+                {t('credit-method.card-number')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="Enter your address" size="large" />
+            <Input placeholder={t('credit-method.card-number-placeholder')} size="large" />
           </S.FormField>
 
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Expiration date
+                {t('credit-method.expiration')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="MM/YYYY" size="large" />
+            <Input placeholder={t('credit-method.expiration-placeholder')} size="large" />
           </S.FormField>
         </S.GroupField>
 
@@ -112,21 +116,21 @@ const CreditCardMethod = () => {
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Name on card
+                {t('credit-method.name-on-card')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="Enter your name on card" size="large" />
+            <Input placeholder={t('credit-method.name-placeholder')} size="large" />
           </S.FormField>
 
           <S.FormField style={{ flex: 1 }}>
             <Typography fontWeight={fontWeight.medium}>
               <S.FormInput>
-                Security code
+                {t('credit-method.security-code')}
                 <Image preview={false} src={icValid} height={23} width={7} />
               </S.FormInput>
             </Typography>
-            <Input placeholder="CVV" size="large" />
+            <Input placeholder={t('credit-method.security-placeholder')} size="large" />
           </S.FormField>
         </S.GroupField>
       </S.Section>
