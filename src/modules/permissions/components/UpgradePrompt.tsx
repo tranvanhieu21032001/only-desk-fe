66eb92f2818ff58755b4b729c3093f5aaa78adf3
tranvanhieu21032001 <tranvanhieu21032001@gmail.com>
2025-08-18 22:42:25 +0700

    build css module

--- src/modules/permissions/components/UpgradePrompt.tsx
+++ src/modules/permissions/components/UpgradePrompt.tsx
@@ -1,42 +1,16 @@
 import React, { ReactNode, useState } from 'react';
 import { Tooltip, Button, Modal } from 'antd';
-import styled from 'styled-components';
 import ToastMessage from '@/shared/components/common/ToastMessage';
 import { ToastMessageType } from '@/shared/helper/enums/common';
 import { toast } from 'react-toastify';
 import ChoisePlan from '@/modules/settings/components/billing/billing-content/main/change-plan/Content/choise-plan/ChoisePlan';
 
+import styles from './UpgradePrompt.module.css';
+
 interface UpgradePromptProps {
   children: ReactNode;
   message: string;
 }
-
-const Wrapper = styled.div`
-  display: inline-block;
-  cursor: pointer;
-  opacity: 0.9;
-  transition: opacity 0.2s;
-  width:100%;
-  &:hover {
-    opacity: 0.6;
-  }
-`;
-
-const TooltipContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 12px;
-  color: #ffff;
-  padding:8px 12px;
-`;
-
-const TooltipMessage = styled.div`
-  flex: 1;
-  font-size: 14px;
-  line-height: 1.4;
-  white-space: normal;
-`;
 
 export const UpgradePrompt: React.FC<UpgradePromptProps> = ({
   children,
@@ -54,8 +28,8 @@ export const UpgradePrompt: React.FC<UpgradePromptProps> = ({
   };
 
   const tooltipContent = (
-    <TooltipContent>
-      <TooltipMessage>🔒 {message}</TooltipMessage>
+    <div className={styles.tooltipContent}>
+      <div className={styles.tooltipMessage}>🔒 {message}</div>
       <Button
         type="primary"
         size="small"
@@ -66,12 +40,16 @@ export const UpgradePrompt: React.FC<UpgradePromptProps> = ({
       >
         Upgrade
       </Button>
-    </TooltipContent>
+    </div>
   );
 
   return (
     <>
       <Tooltip
+        classNames={{
+          root: 'tooltipUpgrade',
+          body: 'tooltipBody',
+        }}
         title={tooltipContent}
         placement="top"
         color="#fff"
@@ -81,18 +59,20 @@ export const UpgradePrompt: React.FC<UpgradePromptProps> = ({
           triggerNode.parentElement || document.body
         }
       >
-        <Wrapper onClick={handleClick}>{children}</Wrapper>
+        <div className={styles.wrapper} onClick={handleClick}>
+          {children}
+        </div>
       </Tooltip>
 
       <Modal
         title="Upgrade Plan"
         open={open}
         onCancel={() => setOpen(false)}
-        footer=''
+        footer=""
         width={1300}
         style={{ top: 20 }}
       >
-        <ChoisePlan/>
+        <ChoisePlan />
       </Modal>
     </>
   );
