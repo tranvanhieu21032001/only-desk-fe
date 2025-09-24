e3af04c3d9ee5ebd5299eed0eec4ab6ab64f9601
tranvanhieu20032001 <tranvanhieu20032001@gmail.com>
2025-09-25 01:00:51 +0700

    feat/add-feature-upgrade

--- src/shared/components/common/PermissionWarningMessage/PermissionWarningMessage.tsx
+++ src/shared/components/common/PermissionWarningMessage/PermissionWarningMessage.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import icWarning from "@/assets/icons/common/ic-warning-message.svg";
+import { Button, Modal } from "antd";
+import ChoisePlan from "@/modules/settings/components/billing/billing-content/main/change-plan/Content/choise-plan/ChoisePlan";
 
 interface PermissionWarningMessageProps {
   message?: string;
@@ -9,16 +11,22 @@ interface PermissionWarningMessageProps {
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 8px;
   background-color: #fff3cd;
   color: #333333;
-  padding: 18px 12px;
-  border:1px solid #faad14;
+  padding: 12px;
+  border: 1px solid #faad14;
   border-radius: 6px;
   font-size: 16px;
   line-height: 1.4;
   font-weight: 500;
-  margin-bottom:24px;
+  margin-bottom: 24px;
+`;
+const ContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
 `;
 
 const Icon = styled.img`
@@ -30,11 +38,37 @@ const Icon = styled.img`
 const PermissionWarningMessage: React.FC<PermissionWarningMessageProps> = ({
   message = "This feature is not available in your current plan",
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <Wrapper role="alert">
-      <Icon src={icWarning} alt="warning" />
-      <span>{message}</span>
-    </Wrapper>
+    <>
+      <Wrapper role="alert">
+        <ContentWrapper>
+          <Icon src={icWarning} alt="warning" />
+          <span>{message}</span>
+        </ContentWrapper>
+        <Button
+          type="primary"
+          size="middle"
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsModalOpen(true);
+          }}
+        >
+          Upgrade
+        </Button>
+      </Wrapper>
+      <Modal
+        title="Upgrade Plan"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={null}
+        width={1300}
+        style={{ top: 20 }}
+      >
+        <ChoisePlan />
+      </Modal>
+    </>
   );
 };
 
